a1c8a40db24cd56e0996bdcd342d40d889bdb34c
Dysthesis <antheoraviel@protonmail.com>
2025-09-20 12:56:34 +1000

    Restructure CLI

--- src/lib/cli.rs
+++ src/lib/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use url::Url;
 #[derive(Parser, Debug)]
 #[command(
@@ -7,23 +7,6 @@ use url::Url;
     about = "Fetch information on a webpage on the command line"
 )]
 pub struct Cli {
-    #[command(subcommand)]
-    pub subcommand: Command,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Command {
-    Add {
-        /// The bookmark to the URL.
-        url: Url,
-
-        /// What tags to add to the bookmark
-        #[arg(short = 't', long)]
-        tags: Vec<String>,
-
-        /// Optional explicit name for the entry. If not, provided, we will attempt to fetch the
-        /// metadata from the page.
-        #[arg(short = 'T', long)]
-        title: Option<String>,
-    },
+    /// The bookmark to the URL.
+    pub url: Url,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
-use std::{collections::HashSet, env, path::PathBuf};
-
 use clap::Parser;
-use libspy::{cli::Cli, entry::Entry, tag::Tag};
-
-pub const DEFAULT_DATA_DIR: &str = "~/.local/share/spy";
+use libspy::{cli::Cli, entry::Entry};
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
     let cli = Cli::parse();
     // Where we store our data
-    dbg!(&cli);
-    match cli.subcommand {
-        libspy::cli::Command::Add { url, tags, title } => {
-            let tag_set: HashSet<Tag> = tags
-                .iter()
-                .filter_map(|t| Tag::try_from(t.as_str()).ok())
-                .collect();
-            match Entry::new(&url, title, Some(tag_set)) {
-                Ok(entry) => {
-                    println!("{}", serde_json::to_string(&dbg!(entry))?);
-                }
-                Err(e) => {
-                    eprintln!("Failed to add entry: {e}");
-                }
-            }
+    match Entry::new(&cli.url, None, None) {
+        Ok(entry) => {
+            println!("{}", serde_json::to_string(&entry)?);
+        }
+        Err(e) => {
+            eprintln!("Failed to add entry: {e}");
         }
     }
+
     Ok(())
 }
